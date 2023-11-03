221bb46d905aa169ec2f2528b069c8234e5a1a4d
knetsteller <kelvesoad@hotmail.com>
2023-11-03 11:22:48 -0300

    initial commit

---
+++ raspberry_serial_arduino.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import serial
+import time
+
+ser = serial.Serial("/dev/ttyACM0", 9600, timeout=5)
+
+input_str = ser.readline()
+print("Read input " + input_str.decode("utf-8").strip() + " from arduino")
+
+while 1:
+    ser.write(b"status\n")
+    input_str = ser.readline().decode("utf-8").strip()
+    if input_str == "":
+        print(".")
+    else:
+        print("Read input back: " + input_str)
+
+    time.sleep(5)
+
+    ser.write(b"set on\n")
+    input_str = ser.readline().decode("utf-8").strip()
+    if input_str == "":
+        print(".")
+    else:
+        print("Read input back: " + input_str)
